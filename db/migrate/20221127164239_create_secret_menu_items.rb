caaa1b0478e0a8a37280b8bb124a8051e0071def
Megan Ratto <mratto@inspirecleanenergy.com>
2022-11-27 11:47:12 -0500

    Migrate table

--- db/migrate/20221127164239_create_secret_menu_items.rb
+++ db/migrate/20221127164239_create_secret_menu_items.rb
@@ -1,8 +1,9 @@
-class CreateSecretMenuItems < ActiveRecord::Migration[7.0]
+class CreateSecretMenuItems < ActiveRecord::Migration[6.0]
   def change
     create_table :secret_menu_items do |t|
-
-      t.timestamps
+      t.string :menu_name
+      t.string :restaurant_name
+      t.string :menu_description
     end
   end
 end
